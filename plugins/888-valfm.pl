49dbb227bb3c5ab7a1a6d2a63f3c398794708dfe
Trizen <trizenx@gmail.com>
2014-04-27 00:48:50 +0300

    Updated the plugin for val.fm;

--- plugins/888-valfm.pl
+++ plugins/888-valfm.pl
@@ -7,10 +7,11 @@
  code      => sub {
      my ($content) = @_;
 
-     if ($content =~ m{class="song-text(.*?)</div>}si) {
+     if ($content =~ m{class="song-text.*?>(.*?)</div>}si) {
          my $lyrics = $1;
          $lyrics =~ s{<p\s+class=['"]verse['"]>}{\n\n}gi;
          $lyrics =~ s{<.*?>}{}sg;
+         return if unpack('A*', $lyrics) eq '';
          return $lyrics;
      }
 
