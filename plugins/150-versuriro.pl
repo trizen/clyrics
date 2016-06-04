e59d38bd234a5b8f9c69bf69d00c17451cc1232d
trizen <trizenx@gmail.com>
2016-06-04 15:44:09 +0300

    fix:   plugins/150-versuriro.pl

--- plugins/150-versuriro.pl
+++ plugins/150-versuriro.pl
@@ -7,7 +7,7 @@
  code      => sub {
      my ($content) = @_;
 
-     if ($content =~ m{</p><br\h*/>(.*?)<(?:script|div)\b}si) {
+     if ($content =~ m{<div id="pagecontent">.*?</script>(.*?)<!-- }si) {
          my $lyrics = $1;
          $lyrics =~ s{<.*?>}{}sg;
          return decode_entities($lyrics);
