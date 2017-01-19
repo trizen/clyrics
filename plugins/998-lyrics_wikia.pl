7f4c0a66a441258e962469ecbbdecb5b69917630
trizen <trizenx@gmail.com>
2017-01-19 22:42:03 +0200

    Fixed the 998-lyrics_wikia.pl plugin.

--- plugins/998-lyrics_wikia.pl
+++ plugins/998-lyrics_wikia.pl
@@ -10,9 +10,10 @@ scalar {
     code => sub {
         my ($content) = @_;
 
-        if ($content =~ m{<div\h+class='lyricbox'.*?</div>(.*?)<!--}is) {
+        if ($content =~ m{<div\b.*? class='lyricbox'.*?>(.*?)</div>}is) {
             my $lyrics = $1;
             $lyrics =~ s{<br\h*/?>}{\n}gi;
+            $lyrics =~ s/<.*?>//gs;
             return decode_entities($lyrics);
         }
 
