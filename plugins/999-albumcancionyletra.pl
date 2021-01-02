ce89bf1a1d5975a976505cc7ce2a5d7ae2640d5a
trizen <trizen@protonmail.com>
2021-01-02 21:41:10 +0200

    Fixed the "albumcancionyletra" plugin. (https://github.com/trizen/clyrics/issues/18)

--- plugins/999-albumcancionyletra.pl
+++ plugins/999-albumcancionyletra.pl
@@ -10,9 +10,12 @@ scalar {
     code => sub {
         my ($content) = @_;
 
-        if ($content =~ m{<div class="letra">(.*?)</div>}si) {
+        if ($content =~ m{<div class="letra">(.*?</script>)\s*</div>}si) {
             my $lyrics = $1;
+            $lyrics =~ s{<script>(.*?)</script>}{}gs;
+            $lyrics =~ s{<br\h*/?>}{\n}gi;
             $lyrics =~ s{<.*?>}{}sg;
+            $lyrics =~ s/^\s+//;
             return $lyrics;
         }
 
